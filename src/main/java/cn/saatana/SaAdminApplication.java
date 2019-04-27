66eb7a4e39ea75c2fe8fea1894f017fc0ed0b0a5
xiangwk <1041367524@qq.com>
2019-04-27 11:44:05 +0800

    V2.0.0 init

---
+++ src/main/java/cn/saatana/SaAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.saatana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaAdminApplication.class, args);
+	}
+
+}
